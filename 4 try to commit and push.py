2cbc309787e2103968fef2388885e295efbb0c5e
LckBastrd <urtutor@yandex.ru>
2023-01-25 18:00:34 +0300

    4 try to push :')

---
+++ 4 try to commit and push.py
@@ -0,0 +1,1 @@
+4 try to commit and push
--- Untitled-1.py
+++
@@ -1,9 +0,0 @@
-class Student:
-    def __init__(self, name, haeight, age)
-        self.name = name
-        self.height = height
-        self.age = age
-
-std1 = Student(self.heigth=1.8, self.age=20, self.name='Anne Lee')
-
-print(std1)
